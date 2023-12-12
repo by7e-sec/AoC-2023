a63bfb4bff29f8fb9d54ebab5762e25c2f2fdf89
By7e.sec <by7e.sec@protonmail.com>
2023-12-12 21:04:36 +0100

    Day 4 pt. 1

---
+++ 4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+
+data = open("ref4.txt").readlines()
+
+def get_points(data):
+    out = 1
+    if len(data) == 1:
+        return out
+
+    for item in data:
+        out *= 2
+
+    return int(out / 2)
+    
+
+calc = []
+for row in data:
+    row = row.strip().split(":")[1]
+    cards = row.split("|")
+    my_card = set(re.split("\s+", cards[0].strip()))
+    their_card = set(re.split("\s+", cards[1].strip()))
+    res = their_card.intersection(my_card)
+    if len(res):
+        # calc.append(res)
+        calc.append(get_points(res))
+
+print(sum(calc))
+# print(calc)
